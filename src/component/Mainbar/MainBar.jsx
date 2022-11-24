8830316d913c35170b2858fdb339714d426d55ac
nima babaei <nimani1378@gmail.com>
2022-11-24 23:42:54 +0330

    fix responsive bugs

--- src/component/Mainbar/MainBar.jsx
+++ src/component/Mainbar/MainBar.jsx
@@ -15,25 +15,34 @@ const MainBar = ({ cityWeather, currency }) => {
     
     return (
         <Grid sx={{
-            height: '100vh',
+            minHeight : '100vh',
+            
         }} item md={7} xs={12}>
             <Stack sx={{
-                height: '48vh'
+                height: {
+                    xs : '30vh',
+                    md : '48vh'}
             }} />
             {/* <Button onClick={()=>{toggleColorMode()}}>toggle</Button> */}
             <Stack direction="row" gap={2} sx={{
                 justifyContent : 'center',
                 alignItems: 'center',
-                marginBottom : '2vh'
+                marginBottom : '2vh',
+                flexWrap : 'wrap'
             }}>
-                <Typography variant="h1" sx={{
+                <Typography variant="h1" align="center" sx={{
                     color: 'text.primary',
                     fontSize : {
-                        xs : '40px',
+                        xs : '70px',
                         sm : '70px',
                         md : '70px',
                         lg : '100px'
+                    },
+                    width : {
+                        xs : '100%',
+                        sm : 'auto'
                     }
+
                 }}>
                     {`${cityWeather?.main?.temp}°`}
                 </Typography>
@@ -42,7 +51,7 @@ const MainBar = ({ cityWeather, currency }) => {
                 }}>
                     <Typography variant="h3" sx={{
                         fontSize : {
-                            xs : '20px',
+                            xs : '40px',
                             sm : '40px',
                             md : '40px',
                             lg : '50px',
